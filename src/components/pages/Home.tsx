8ba4de4d2e0cd419fd402d2c6da9831d977c28ba
Denis Mosin <mosin-denis@bk.ru>
2025-06-16 23:03:32 +0700

    Add Tickets

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -1,10 +1,11 @@
 import Dropdown from '../Dropdown';
-
+import Tickets from '../Tickets.tsx'
 const Home = () => {
   return (
     <div className="min-h-screen bg-[#2C2D30] flex flex-col items-center justify-center gap-6 p-10 text-white">
       <h1 className="text-6xl font-bold text-primary-500 text-center">Home</h1>
       <Dropdown/>
+        <Tickets/>
     </div>
   );
 };
