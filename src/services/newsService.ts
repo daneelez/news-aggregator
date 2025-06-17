0fa44c6b0e775defb60bd4e73bc4c2520ef37fd3
Даниил Жилин <Daniil.Zhl@yandex.ru>
2025-06-17 16:05:01 +0700

    refactor

--- src/services/newsService.ts
+++ src/services/newsService.ts
@@ -5,7 +5,6 @@ export async function fetchNewsByTicker(ticker: string, page: number) {
         const res = await api.get("https://gnews.io/api/v4/search", {
             params: {
                 q: ticker,
-                token: "711aa176be0081fdea8b68ad5b4edfb3",
                 lang: "ru",
                 max: 5,
                 page,
